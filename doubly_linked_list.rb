609c3b88d7c7fad9c30d73467e532a6dc1424b0b
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-20 12:44:48 -0400

    Write node class inside doubly linked list file

--- doubly_linked_list.rb
+++ doubly_linked_list.rb
@@ -1,4 +1,13 @@
-require './node.rb'
+class Node
+    attr_reader :value
+    attr_accessor :next_node, :prev_node
+
+    def initialize(value, next_node = nil, prev_node = nil)
+        @value = value
+        @next_node = next_node
+        @prev_node = prev_node
+    end
+end
 
 class DoublyLinkedList
     def initalize()
