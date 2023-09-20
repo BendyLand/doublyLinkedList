a232dd996f16b8ec32225ca6c5b243b6b0c925d8
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-20 12:48:06 -0400

    Write Node class inside DoublyLinkedList.cs

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -1,6 +1,24 @@
 using System;
 using System.Collections.Generic;
 
+class Node<T>
+{
+    public T Value
+    { get; private set; }
+    public Node<T> NextNode
+    { get; set; }
+    public Node<T> PrevNode
+    { get; set; }
+
+    public Node(T value, Node<T> nextNode = null, Node<T> prevNode = null)
+    {
+        Value = value;
+        NextNode = nextNode;
+        PrevNode = prevNode;
+    }
+}
+
+
 class DoublyLinkedList<T>
 {
     public Node<T> HeadNode
--- node.rb
+++
@@ -1,9 +0,0 @@
-class Node
-    attr_reader :value
-    attr_accessor :next_node, :prev_node    
-    def initialize(value, next_node = nil, prev_node = nil)
-        @value = value
-        @next_node = next_node
-        @prev_node = prev_node
-    end
-end
